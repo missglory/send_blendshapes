b74f620549162624a03f9807dbebb509851c8e3d
missglory <xhaustful@gmail.com>
2019-06-18 13:32:21 +0300

    Initial Commit

---
+++ tcp_swift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tcp_swift
+//
+//  Created by MacMini on 18/06/2019.
+//  Copyright © 2019 MacMini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
